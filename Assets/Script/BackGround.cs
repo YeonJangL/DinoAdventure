c5cfd00b3318fd9989fb1744cf83a3f2511bf568
YeonJangL <lej32552002@gmail.com>
2024-04-02 17:53:27 +0900

    비타 이동 스크립트 마저 수정하기

--- Assets/Script/BackGround.cs
+++ Assets/Script/BackGround.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class BackGround : MonoBehaviour
 {
+    // 스크롤이 되는 백그라운드
+    public float scrollSpeed = 0.3f;
+    private Material mymaterial;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mymaterial = GetComponent<Renderer>().material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 오프셋 머터리얼에서 가져오기
+        Vector2 newOffset = mymaterial.mainTextureOffset;
+
+        // 새롭게 offset 바꾸기
+        // x 부분 값을 속도에 프레임 보정해서 더해줌
+        newOffset.Set(newOffset.x + (scrollSpeed * Time.deltaTime), 0);
+
+        // 머터리얼에 오프셋 값 넣어주기
+        mymaterial.mainTextureOffset = newOffset;
     }
 }
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public AudioClip dieClip;
+    public float jump = 7.0f;
+
+    private int deathCnt = 0;
+    private bool isGrounded = false;
+    private bool isDead = false;
+
+    private Rigidbody2D playerRbody;
+    private Animator animator;
+    private AudioSource playerAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 사용할 컴포넌트 가져와서 변수에 할당
+        playerRbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 점프
+        if (isDead) // 이미 죽었으면 리턴
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRbody.velocity = Vector2.zero; // 직전 속도에 영향 받지 않기 위해서 점프할때 속도를 0으로 함
+            playerRbody.AddForce(new Vector2 (0, 0));
+            playerAudio.Play();
+        }
+
+        animator.SetBool("Gorounded", isGrounded);
+    }
+
+    private void Die()
+    {
+        // 죽을때
+        animator.SetTrigger("Die");
+
+        playerAudio.clip = dieClip;
+        playerAudio.Play();
+
+        playerRbody.velocity = Vector2.zero;
+
+        isDead = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 적이랑 충돌 감지
+        if (collision.gameObject.CompareTag("Enemy") && !isDead)
+        {
+            Die();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // 바닥 충돌 감지
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        // 점프 감지
+        isGrounded = false;
+    }
+}
